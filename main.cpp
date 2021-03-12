076993da63809d1000dcc020c65ec13bdcd41741
Intellb <nicolas.rnbp@gmail.com>
2021-03-12 14:13:29 -0500

    Finalfinalisimo

--- main.cpp
+++ main.cpp
@@ -3,50 +3,50 @@
 #include<fstream>
 #include<string>
 #include<chrono>
-#include <time.h>  
 #include<conio.h>
+#include<windows.h>
 using namespace std;
+using namespace std::chrono;
 //VARIABLES GLOBALES
-struct Celda{
-    bool visitado=false;
-    bool paredes[4]={true,true,true,true};
+struct Celda{                   //Estructura basica para la generacion del laberinto
+    bool visitado=false;    
+    bool paredes[4]={true,true,true,true}; //Arriba, derecha, abajo,izquierda
 };
-Celda celdas[30][30];
-char laberinto[61][61];
-vector<int> recuri, recurj;
-string nombres[3];
-int max_punt[3];
-
-//Declaracion Funciones
-int menu();
-void instrucciones();
-void jugar();
-void max_puntajes();
-void creditos();
-
-
-
-
-void leerpuntajes(){
-    string line;
-    int i=0, index;
-    ifstream puntajes("max_puntajes.txt");
-    while (getline (puntajes, line)){
-        index=line.find(",");
-        nombres[i]=line.substr(index+1,line.size()-index); 
-        max_punt[i]=stoi(line.substr(0,index));
-        i++;
-    }   
-    puntajes.close();
-}
-
-
-
+Celda celdas[15][15];           //Matriz de struct para la generacion 
+char laberinto[31][31];         //Matriz de caracteres para impresion de laberinto
+vector<int> recuri, recurj;     //Stacks de posiciones, para backtrack
+string nombres[3];              //Vector de nombres correspondientes a puntajes maximos
+int max_punt[3];                //Vector de puntajes maximos
+//DECLARACION FUNCIONES
+void globalreset(int n);        //Devuelve las variables globales a su estado inicial
+//Usuario
+void jugar();                   //Ejecuta y controla el juego laberinto
+void mover_jugador(int &i, int &j, int n, bool &gameover);  //Controla y muestra la posicion del jugador
+void puntmaximo(int puntos);    //Inserta el puntaje obtenido de la secion si este se califica dentro de puntaje maximo
+//Laberinto
+void crearlaberinto(int n);     //Controla la generacion del laberinto bajo el algoritmo de backtraking
+bool VecinoVisitados(int i, int j, int n);  //Identifica si una celda tiene vecinos sin visitar
+void mover(int &i, int &j, int n);  //Mueve aleatoriamente la posicion a una celda no visitada
+//Imprimir Informacion
+int menu();                 //Opciones, retorna una opcion valida
+void instrucciones();       //Instrucciones para jugar
+void max_puntajes();        //Top 3 puntajes de maxpuntajes.txt
+void creditos();            //Autores del programa
+//Manipulacion de archivos
+void leerpuntajes();        //Lee documento y extrae informacion de puntajes maximos
+void guardar();             //Guarda puntajes y nombres en un documento
+//Manipulacion de terminal
+void cursor_coor(int i, int j); //Posiciona el cursor de la termninal en un lugar de la pantalla
+    //Tomadas de la documentacion de Windows
+void Mostrarcursor(bool bandera);   //Remueve el crusor de la termial
+void DimVentana(int Width, int Height);      //Dimensiona la ventana de la terminal
+//FUNCION PRINCIPAL
 int main(){
     bool go=true;
     int opcion;
     leerpuntajes();
     while(go){
+        DimVentana(50,25);
         opcion=menu();
         switch(opcion){
             case 1:
@@ -68,123 +68,124 @@ int main(){
     }
     return 0;
 }
-void cursor_coor(int i, int j){
-    printf("\e[%d;%df",i,j);
+//FUNCIONES
+void leerpuntajes(){ 
+    string line;
+    int i=0, index;
+    ifstream puntajes("max_puntajes.txt");
+    while (getline (puntajes, line)){
+        index=line.find(",");
+        nombres[i]=line.substr(index+1,line.size()-index); 
+        max_punt[i]=stoi(line.substr(0,index));
+        i++;
+    }   
+    puntajes.close();
 }
-
-
 int menu(){
     int opcion;
     int valido=true;
     system ("CLS");
-    cout<<"Menu"<<endl;
-    cout<<"1: Jugar"<<endl;
-    cout<<"2: Puntajes Maximos"<<endl;
-    cout<<"3: Creditos"<<endl;
-    cout<<"4: Salir"<<endl;
-    cout<<"Opcion: ";
+    cursor_coor(4, 22);cout<<"Menu";
+    cursor_coor(6, 5);cout<<"1: Jugar";
+    cursor_coor(8, 5);cout<<"2: Puntajes Maximos";
+    cursor_coor(10, 5);cout<<"3: Creditos";
+    cursor_coor(12, 5);cout<<"4: Salir";
+    cursor_coor(14, 5);cout<<"Opcion: ";
     cin>>opcion;
-    while(valido){
+    while(valido){ // Valida inputs de tipo diferente a entero
         while(cin.fail()){
-            cin.clear();
+            cin.clear(); 
             cin.ignore(256,'\n');
-            cursor_coor(6, 0);cout<<"\e[2K"<<"Ingrese un valor entero\n"; // \e[2K  escape code para borrar linea
-            cout<<"\e[2K"<<"Opcion: ";cin >> opcion;
+            cursor_coor(14, 5);cout<<"\e[2K"<<"Ingrese un valor entero\n"; // \e[2K  escape code para borrar linea
+            cout<<"\e[2K"<<"    Opcion: ";cin >> opcion;
         }
         if(opcion<0 or opcion>4){
-            cursor_coor(6, 0);cout<<"\e[2K"<<"Ingrese una opcion valida\n";
-            cout<<"\e[2K"<<"Opcion: ";cin >> opcion;
+            cursor_coor(14, 5);cout<<"\e[2K"<<"Ingrese una opcion valida\n";
+            cout<<"\e[2K"<<"    Opcion: ";cin >> opcion;
         }else{
             valido=false;
         }
     }
     return opcion;
 }
-
-
+void jugar(){
+    bool gameover=false;
+    int nivel=1, mapsize=10, puntaje=0;
+    instrucciones();
+    Mostrarcursor(false);
+    while (!gameover and nivel<=5){
+        int pji=1,pjj=1;
+        system("CLS");
+        DimVentana(2*(mapsize*2+1),mapsize*2+2);
+        if(nivel==5){
+            cursor_coor((2*mapsize+1)/2-1,5);cout<<"Ultimo nivel\n";
+        }
+        cursor_coor((2*mapsize+1)/2,5);cout<<"Nivel: "<<nivel<<"\n\n"; 
+        system("Pause");
+        crearlaberinto(mapsize);
+        cursor_coor((2*mapsize+1)*2,0);cout<<"Puntaje: "<<puntaje;
+        auto Tiempo0=steady_clock::now();
+        mover_jugador(pji,pjj,mapsize,gameover);
+        if(!gameover){
+            auto TiempoF=steady_clock::now();
+            globalreset(mapsize);
+            duration<double> deltaT=TiempoF-Tiempo0;
+            puntaje=puntaje+nivel*100-deltaT.count();
+            nivel++;
+            mapsize++;
+        }
+    }
+    Mostrarcursor(true);
+    globalreset(mapsize);
+    system("CLS");
+    cursor_coor((2*mapsize+1)/2,5);cout<<"Fin de juego\n";
+    puntmaximo(puntaje);
+    system("Pause");
+}
+void max_puntajes(){
+    system ("CLS");
+    cursor_coor(2,17);cout<<"Puntajes Maximos\n";
+    cursor_coor(4,3);cout<<"Nombre ";
+    cursor_coor(4,34);cout<<"Puntaje "<<endl;
+    for(int i=0; i<3; i++){
+        cursor_coor(6+i*2,4);cout<<nombres[i]; 
+        cursor_coor(6+i*2,35);cout<<max_punt[i];
+    }   
+    cursor_coor(12,0);system("PAUSE");
+}
+void creditos(){
+    system ("CLS");
+    cursor_coor(2,12);cout<<"Fundamentos de programacion";
+    cursor_coor(4,13);cout<<"Proyecto Final: Laberinto";
+    cursor_coor(6,5);cout<<"Grupo #3:";
+    cursor_coor(8,5);cout<<"Burbano Nicolas";
+    cursor_coor(10,5);cout<<"Salome Garzon";
+    cursor_coor(12,5);cout<<"Feria Danna";
+    cursor_coor(14,5);cout<<"Galo Lopez\n\n";
+    system("PAUSE");
+}
 void instrucciones(){
     system ("CLS");
+    DimVentana(45,15);
     cout<<"Instrucciones para jugar"<<endl;
-    cout<<"Cruza los 10 niveles lo mas rapido posible"<<endl;
+    cout<<"Cruza los 5 niveles lo mas rapido posible"<<endl;
     cout<<"Cada nivel aumenta el multiplicador de puntos"<<endl;
+    cout<<"Cada segundo RESTA puntaje"<<endl;
+    cout<<"I: Jugador"<<endl;
+    cout<<"X: Objetivo"<<endl;
+    cout<<"#: Paredes"<<endl;
     cout<<"Controles: "<<endl;
-    cout<<"W: moverse para arriba"<<endl;
-    cout<<"S: moverse para abajo"<<endl;
-    cout<<"D: moverse para derecha"<<endl;
-    cout<<"A: moverse para izquierda"<<endl;
+    cout<<"w: moverse para arriba"<<endl;
+    cout<<"s: moverse para abajo"<<endl;
+    cout<<"d: moverse para derecha"<<endl;
+    cout<<"a: moverse para izquierda"<<endl;
     cout<<"ESC: Salir"<<endl;
     system("PAUSE");
 }
-
-void mover(int &i, int &j,int n){
-    int mov;
-    bool cambiocelda=false;;
-    while (!cambiocelda){
-        mov=rand()%4;
-        switch (mov){
-            case 0:
-                if(i-1>=0){
-                    if(celdas[i-1][j].visitado==false){
-                        celdas[i][j].paredes[0]=false;
-                        celdas[i-1][j].paredes[2]=false;
-                        celdas[i-1][j].visitado=true;
-                        cambiocelda=true;
-                        i--;
-                    }
-                }
-                break;
-            case 1:
-                if(j+1<n){
-                    if(celdas[i][j+1].visitado==false){
-                        celdas[i][j].paredes[1]=false;
-                        celdas[i][j+1].paredes[3]=false;
-                        celdas[i][j+1].visitado=true;
-                        cambiocelda=true;
-                        j++;
-                    }
-                }
-                break;
-            case 2:
-                if(i+1<n){
-                    if(celdas[i+1][j].visitado==false){
-                        celdas[i][j].paredes[2]=false;
-                        celdas[i+1][j].paredes[0]=false;
-                        celdas[i+1][j].visitado=true;
-                        cambiocelda=true;
-                        i++;
-                    }
-                }
-                break;
-            case 3:
-                if(j-1>=0){
-                    if(celdas[i][j-1].visitado==false){
-                        celdas[i][j].paredes[3]=false;
-                        celdas[i][j-1].paredes[1]=false;
-                        celdas[i][j-1].visitado=true;
-                        cambiocelda=true;
-                        j--;
-                    }
-                }
-                break;
-        }
-    }
-    recuri.push_back(i); recurj.push_back(j);
-}
-
-bool VecinoVisitados(int i, int j, int n){
-    int mov[]={1,1,1,1};
-    if(i-1<0) mov[0]=0;
-    if(j+1==n) mov[1]=0;
-    if(i+1==n) mov[2]=0;
-    if(j-1<0) mov[3]=0;
-    return celdas[i-mov[0]][j].visitado and celdas[i][j+mov[1]].visitado and celdas[i+mov[2]][j].visitado and celdas[i][j-mov[3]].visitado;  
-                    //arriba                          //derecha                          //abajo                 //izquierda
-}
-
 void crearlaberinto(int n){
     int contador=1;
     int pi,pj;  //posicion 
-    //Algoritmo de generacion de laberinto recursivo
+    //Algoritmo de generacion de laberinto backtracking
     srand (time(NULL));
     pi=rand()%n;
     pj=rand()%n;
@@ -206,7 +207,6 @@ void crearlaberinto(int n){
             laberinto[i][j]='#';
         }
     }
-
     //Remueve paredes
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -244,41 +244,103 @@ void crearlaberinto(int n){
         cout<<endl;
     }
 } 
-
+bool VecinoVisitados(int i, int j, int n){
+    int mov[]={1,1,1,1};
+    if(i-1<0) mov[0]=0;
+    if(j+1==n) mov[1]=0;
+    if(i+1==n) mov[2]=0;
+    if(j-1<0) mov[3]=0;
+    return celdas[i-mov[0]][j].visitado and celdas[i][j+mov[1]].visitado and celdas[i+mov[2]][j].visitado and celdas[i][j-mov[3]].visitado;  
+                    //arriba                          derecha                          abajo                 izquierda
+}
+void mover(int &i, int &j,int n){
+    int mov;
+    bool cambiocelda=false;;
+    while (!cambiocelda){
+        mov=rand()%4;
+        switch (mov){
+            case 0:
+                if(i-1>=0){//Arriba
+                    if(celdas[i-1][j].visitado==false){
+                        celdas[i][j].paredes[0]=false;
+                        celdas[i-1][j].paredes[2]=false;
+                        celdas[i-1][j].visitado=true;
+                        cambiocelda=true;
+                        i--;
+                    }
+                }
+                break;
+            case 1:
+                if(j+1<n){//Derecha
+                    if(celdas[i][j+1].visitado==false){
+                        celdas[i][j].paredes[1]=false;
+                        celdas[i][j+1].paredes[3]=false;
+                        celdas[i][j+1].visitado=true;
+                        cambiocelda=true;
+                        j++;
+                    }
+                }
+                break;
+            case 2:
+                if(i+1<n){//Abajo
+                    if(celdas[i+1][j].visitado==false){
+                        celdas[i][j].paredes[2]=false;
+                        celdas[i+1][j].paredes[0]=false;
+                        celdas[i+1][j].visitado=true;
+                        cambiocelda=true;
+                        i++;
+                    }
+                }
+                break;
+            case 3:
+                if(j-1>=0){//Izquierda
+                    if(celdas[i][j-1].visitado==false){
+                        celdas[i][j].paredes[3]=false;
+                        celdas[i][j-1].paredes[1]=false;
+                        celdas[i][j-1].visitado=true;
+                        cambiocelda=true;
+                        j--;
+                    }
+                }
+                break;
+        }
+    }
+    recuri.push_back(i); recurj.push_back(j);
+}
 void mover_jugador(int &i, int &j, int n, bool &gameover){
     char input;
     while((i!=2*n-1 or j!=2*n-1) and !gameover){    
         input=getch();
         switch (input){
-            case 'w':
+            case 'w' ://Arriba
                 if(laberinto[i-1][j]!='#'){
                     cursor_coor(i+1,2*j+1);cout<<' ';
                     i--;
                     cursor_coor(i+1,2*j+1);cout<<'I';
                 }
                 break;
-            case 'd':
-                if(laberinto[i][j+1]!='#'){
+            case 'd' :
+                if(laberinto[i][j+1]!='#'){ //derecha
                     cursor_coor(i+1,2*j+1);cout<<' ';
                     j++;
                     cursor_coor(i+1,2*j+1);cout<<'I';
                 }
                 break;
             case 's':
-                if(laberinto[i+1][j]!='#'){
+                if(laberinto[i+1][j]!='#'){//abajo
                     cursor_coor(i+1,2*j+1);cout<<' ';
                     i++;
                     cursor_coor(i+1,2*j+1);cout<<'I';
                 }
                 break;
             case 'a':
-                if(laberinto[i][j-1]!='#'){
+                if(laberinto[i][j-1]!='#'){//izquierda
                     cursor_coor(i+1,2*j+1);cout<<' ';
                     j--;
                     cursor_coor(i+1,2*j+1);cout<<'I';
                 }
                 break;
-            case '\e':
+            case '\e': //salir del juego
                 gameover=true;
                 break;
         }   
@@ -296,14 +358,6 @@ void globalreset(int n){
     recuri.clear();
     recurj.clear();
 }
-void guardar(){
-    ofstream puntajes("max_puntajes.txt");
-    for(int i=0;i<3;i++){
-        puntajes<<max_punt[i]<<","<<nombres[i]<<endl;
-    }
-    puntajes.close();
-}
-
 void puntmaximo(int puntos){
     int holder;
     string nombre;
@@ -323,54 +377,36 @@ void puntmaximo(int puntos){
     }
     guardar();
 }
-
-void jugar(){
-    bool gameover=false;
-    int nivel=1, mapsize=10, puntaje=0;
-    //instrucciones();
-    while (!gameover and nivel<=5){
-        int pji=1,pjj=1;
-        system("CLS");
-        if(nivel==5) cout<<"Ultimo nivel\n";
-        cout<<"\nNivel: "<<nivel<<endl; 
-        system("Pause");
-        crearlaberinto(mapsize);
-        mover_jugador(pji,pjj,mapsize,gameover);
-        if(!gameover){
-            globalreset(mapsize);
-            puntaje=puntaje+nivel*100;
-            nivel++;
-            mapsize++;
-        }
+void guardar(){
+    ofstream puntajes("max_puntajes.txt");
+    for(int i=0;i<3;i++){
+        puntajes<<max_punt[i]<<","<<nombres[i]<<endl;
     }
-    globalreset(mapsize);
-    system("CLS");
-    cursor_coor(1,1);cout<<"Fin de juego\n";
-    puntmaximo(puntaje);
-    system("Pause");
+    puntajes.close();
 }
+void cursor_coor(int i, int j){
+    printf("\e[%d;%df",i,j); //Codigo ESC para el posicionamiento de cursor en la terminal
+}
+//Funciones a continuacion tomadas de la documentacion de microsoft Windows, https://docs.microsoft.com/en-us/.
+void Mostrarcursor(bool bandera){
+        HANDLE out=GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(out, &cursorInfo);
+        cursorInfo.bVisible = bandera; 
+        SetConsoleCursorInfo(out, &cursorInfo);
+}  
+void DimVentana(int Width, int Height){ 
+    _COORD coord; 
+    coord.X = Width; 
+    coord.Y = Height; 
 
-// Top 3 puntajes de maxpuntajes.txt
-void max_puntajes(){
-    system ("CLS");
-    cout<<"Puntajes Maximos\n";
-    cout<<"Nombre ";
-    cursor_coor(1,30);cout<<"Puntaje "<<endl;
-    for(int i=0; i<3; i++){
-        cout<<nombres[i]; 
-        cursor_coor(2+i,30);cout<<max_punt[i]<<endl;
-    }   
-    system("PAUSE");
-}
+    _SMALL_RECT Rect; 
+    Rect.Top = 0; 
+    Rect.Left = 0; 
+    Rect.Bottom = Height - 1; 
+    Rect.Right = Width - 1; 
 
-void creditos(){
-    system ("CLS");
-    cout<<"Grupo #3"<<endl;
-    cout<<"Burbano Nicolas"<<endl;
-    cout<<"Salome Garzon"<<endl;
-    cout<<"Feria Danna"<<endl;
-    cout<<"Galo Lopez"<<endl;
-    cout<<"Fundamentos de programacion"<<endl;
-    cout<<"Proyecto Final: Laberinto\n\n"<<endl;
-    system("PAUSE");
-}
+    HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);     
+    SetConsoleScreenBufferSize(Handle, coord); 
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);
+} 
